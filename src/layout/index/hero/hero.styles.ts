a0ba11934bc1538a6a9194826ea15e98da71e30d
Raniere Marques <Raniere852@outlook.com>
2022-06-29 15:50:30 -0300

    feat: hero section

--- src/layout/index/hero/hero.styles.ts
+++ src/layout/index/hero/hero.styles.ts
@@ -1,3 +1,76 @@
 import { styled } from 'stitches.config'
+import { ArrowDown as ArrowDownSvg } from './svgs'
 
-export const Container = styled('div', {})
+export const Section = styled('section', {
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
+
+export const Hat = styled('span', {
+  marginTop: 80,
+
+  fontWeight: 400,
+  fontSize: 14,
+  lineHeight: '24px',
+  color: '#E3E5E8',
+})
+
+export const Title = styled('h1', {
+  marginTop: 16,
+
+  fontWeight: 500,
+  fontSize: 40,
+  lineHeight: '56px',
+  textAlign: 'center',
+  color: '#F6F6F8',
+})
+
+export const InputWrapper = styled('div', {
+  marginTop: 32,
+})
+
+export const SearchInput = styled('input', {
+  height: 60,
+})
+
+export const HelperText = styled('span', {
+  margin: '32px 0 112px',
+
+  fontWeight: 300,
+  fontSize: 14,
+  lineHeight: '26px',
+  textAlign: 'center',
+  color: '#F6F6F8',
+})
+
+export const ImageWrapper = styled('div', {
+  position: 'absolute',
+  top: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: -1,
+  userSelect: 'none',
+  pointerEvents: 'none',
+})
+
+export const ImageOverlay = styled('div', {
+  position: 'absolute',
+  top: 0,
+  width: '100%',
+  height: '100%',
+  background: '#0F141A',
+  opacity: 0.7,
+})
+
+export const ArrowDown = styled(ArrowDownSvg, {
+  boxSizing: 'content-box',
+  padding: 6,
+  background: '#FFFFFF',
+  boxShadow: '0px 5px 32px rgba(71, 85, 103, 0.5)',
+  borderRadius: 999,
+
+  position: 'absolute',
+  bottom: -16,
+})
--- src/layout/index/hero/hero.tsx
+++ src/layout/index/hero/hero.tsx
@@ -4,13 +4,38 @@ import { downtownHuntsville } from './images'
 
 export function Hero() {
   return (
-    <div>
-      <Image
-        src={downtownHuntsville}
-        alt="Downtown Hunstsville Lake"
-        layout="responsive"
-        priority
-      />
-    </div>
+    <S.Section>
+      <S.Hat>HEARTY WELCOME</S.Hat>
+      <S.Title>
+        Find your dream <br />
+        home today
+      </S.Title>
+
+      <S.InputWrapper>
+        <S.SearchInput
+          type="text"
+          placeholder="Search by address, neighborhood, city or ZIP code"
+        />
+      </S.InputWrapper>
+
+      <S.HelperText>
+        We will make your dream home a reality. <br />
+        Find out more about the various homes we have available today.
+      </S.HelperText>
+
+      <S.ImageWrapper>
+        <Image
+          src={downtownHuntsville}
+          alt="Downtown Hunstsville Lake"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="50% 35%"
+          priority
+        />
+        <S.ImageOverlay />
+      </S.ImageWrapper>
+
+      <S.ArrowDown />
+    </S.Section>
   )
 }
