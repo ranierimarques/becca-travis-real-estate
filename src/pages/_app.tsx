f44c89c4011a0f99c3f7a2f25d5740e5b9212f97
Raniere Marques <Raniere852@outlook.com>
2022-06-28 21:57:05 -0300

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { globalStyles } from '@styles/globals'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Navbar } from 'src/common'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -7,6 +8,28 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width" />
+        <meta charSet="UTF-8" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
