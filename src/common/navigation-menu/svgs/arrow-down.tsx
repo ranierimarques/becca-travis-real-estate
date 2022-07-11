d5a7f198c2de434a1f181dd15b36ffaddbc08cae
Pedro Renan <prenansb@gmail.com>
2022-07-11 09:27:37 -0300

    fix(navigation menu): arrow svg fixed

--- src/common/navigation-menu/svgs/arrow-down.tsx
+++ src/common/navigation-menu/svgs/arrow-down.tsx
@@ -2,13 +2,13 @@ export default function Svg({ ...props }) {
   return (
     <svg
       width="16"
-      height="17"
+      height="16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
-        d="m4 6.5 4 4 4-4"
+        d="m4 6 4 4 4-4"
         stroke="#9D446E"
         strokeLinecap="round"
         strokeLinejoin="round"
