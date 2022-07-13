bf9e430879e125fe95a05c42a180eaa18952b004
Raniere Marques <Raniere852@outlook.com>
2022-07-13 10:22:33 -0300

    fix: bg-color footer disclaimer links

--- src/common/footer/disclaimer-links/disclaimer-links.tsx
+++ src/common/footer/disclaimer-links/disclaimer-links.tsx
@@ -21,7 +21,7 @@ export function DisclaimerLinks() {
 
         padding: '20px 0',
         boxShadow: 'inset 0 1px 0 0 $colors$magenta9, inset 0 -1px 0 0 $colors$magenta9',
-        background: '$magenta7',
+        background: 'rgba($colors$magenta7Rgb, 0.5)',
       }}
     >
       {links.map(link => (
