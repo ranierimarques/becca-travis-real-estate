368b4295bf38af39fc4de3314c55b60426cc36b1
Pedro Renan <prenansb@gmail.com>
2022-07-21 19:00:23 -0300

    feat(why becca): history section added

---
+++ src/layout/why-becca/history/history.styles.ts
@@ -0,0 +1,52 @@
+import { styled } from "stitches.config"
+
+export const Section = styled("section", {
+  maxWidth: "1072px",
+  margin: "0 auto",
+  padding: "155px 0 160px",
+})
+
+export const Hat = styled("span", {
+  marginBottom: 8,
+
+  fontWeight: "500",
+  fontSize: "14px",
+  lineHeight: "24px",
+  letterSpacing: "0.03em",
+  color: "#9D446E",
+})
+
+export const Title = styled("h1", {
+  marginBottom: 16,
+
+  fontWeight: "500",
+  fontSize: "24px",
+  lineHeight: "36px",
+  color: "#58243C",
+})
+
+export const Paragraph = styled("p", {
+  maxWidth: "458px",
+  fontWeight: "400",
+  fontSize: "16px",
+  lineHeight: "28px",
+  color: "#4E4B59",
+})
+
+export const Becca = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+})
+
+export const OurHistory = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+})
+
+export const Family = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+})
---
+++ src/layout/why-becca/history/history.tsx
@@ -0,0 +1,68 @@
+import { Box } from "@common"
+import Image from "next/image"
+import * as S from "./history.styles"
+import { becca, family, ourHistory } from "./images"
+import { PathSvg } from "./svgs"
+
+export function History() {
+  return (
+    <S.Section>
+      <S.Becca>
+        <Box>
+          <S.Hat>- MEET HUNTSVILLE</S.Hat>
+          <S.Title>Becca Travis Real State</S.Title>
+          <S.Paragraph>
+            There are many reasons why Huntsville is a great place to live. The
+            economy is strong and there are plenty of jobs available. The
+            schools are excellent, and the living cost is very affordable.
+          </S.Paragraph>
+        </Box>
+
+        <Box css={{ borderRadius: 8, overflow: "hidden" }}>
+          <Image src={becca} alt="Becca Travis" />
+        </Box>
+      </S.Becca>
+
+      <Box>
+        <PathSvg />
+      </Box>
+
+      <S.OurHistory>
+        <Box css={{ borderRadius: 8, overflow: "hidden" }}>
+          <Image src={ourHistory} alt="Becca Travis" />
+        </Box>
+
+        <Box>
+          <S.Hat>- MEET HUNTSVILLE</S.Hat>
+          <S.Title>Becca Travis Real State</S.Title>
+          <S.Paragraph>
+            Huntsville is home to a number of highly respected colleges and
+            universities. The quality of education in Huntsville is excellent,
+            and students who attend school here have the opportunity to receive
+            a well-rounded education.
+          </S.Paragraph>
+        </Box>
+      </S.OurHistory>
+
+      <Box css={{ transform: "scaleX(-1)" }}>
+        <PathSvg />
+      </Box>
+
+      <S.Family>
+        <Box>
+          <S.Hat>- MEET HUNTSVILLE</S.Hat>
+          <S.Title>Becca Travis Real State</S.Title>
+          <S.Paragraph>
+            The Huntsville area offers an excellent real estate market.
+            You&apos;ll find an amazing selection of homes for sale here, and my
+            job is to make sure you have all the information needed so we can
+            save time on finding what fits best with your needs!
+          </S.Paragraph>
+        </Box>
+        <Box css={{ borderRadius: 8, overflow: "hidden" }}>
+          <Image src={family} alt="Happy Family" />
+        </Box>
+      </S.Family>
+    </S.Section>
+  )
+}
