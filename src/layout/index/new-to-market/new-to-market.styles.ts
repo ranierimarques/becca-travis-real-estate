2c71eb7f49d3179c7472489ca91c340879f1552b
Pedro Renan <prenansb@gmail.com>
2022-08-05 15:50:44 -0300

    refactor(new to market): receiving houses from bridgeapi

--- src/layout/index/new-to-market/new-to-market.styles.ts
+++ src/layout/index/new-to-market/new-to-market.styles.ts
@@ -1,115 +1,135 @@
-import { styled } from 'stitches.config'
+import { styled } from "stitches.config"
+import { FavoriteSvg } from "./svgs"
 
-export const Section = styled('section', {
-  maxWidth: '1072px',
-  margin: '0 auto',
-  padding: '120px 0 160px',
+export const Section = styled("section", {
+  maxWidth: "1072px",
+  margin: "0 auto",
+  padding: "120px 0 160px",
 })
 
-export const Title = styled('h2', {
-  fontWeight: '500',
-  fontSize: '36px',
-  lineHeight: '50px',
-  color: '$magenta6',
+export const Title = styled("h2", {
+  fontWeight: "500",
+  fontSize: "36px",
+  lineHeight: "50px",
+  color: "$magenta6",
 })
 
-export const ViewAll = styled('a', {
-  fontWeight: '500',
-  fontSize: '14px',
-  lineHeight: '36px',
-  color: '$magenta9',
+export const ViewAll = styled("a", {
+  fontWeight: "500",
+  fontSize: "14px",
+  lineHeight: "36px",
+  color: "$magenta9",
 })
 
-export const Houses = styled('ul', {
-  display: 'flex',
-  alignItems: 'center',
+export const Houses = styled("ul", {
+  display: "flex",
+  alignItems: "center",
   gap: 32,
 })
 
-export const House = styled('li', {
-  border: '1px solid $grayW8',
-  borderRadius: '8px',
-  overflow: 'hidden',
+export const House = styled("li", {
+  border: "1px solid #CDD1D6",
+  borderRadius: "8px",
+  overflow: "hidden",
 
-  background: '$offWhite2',
+  background: "$white",
 })
 
-export const New = styled('span', {
-  display: 'block',
-  padding: 5,
-  background: '$red2',
-  borderRadius: '5px',
+export const New = styled("span", {
+  display: "block",
+  padding: "2px 8px",
+  background: "rgba(15, 20, 26, 0.6)",
+  borderRadius: "5px",
 
-  position: 'absolute',
-  top: '16px',
-  left: '16px',
-  zIndex: '2',
+  position: "absolute",
+  top: "-1px",
+  left: "-1px",
+  zIndex: "2",
 
-  fontWeight: '600',
-  fontSize: '12px',
-  lineHeight: '18px',
-  color: '$white',
+  fontWeight: "700",
+  fontSize: "12px",
+  lineHeight: "18px",
+  color: "$white",
 })
 
-export const HouseInfo = styled('div', {
-  padding: '14px 0 16px 16px',
+export const HeartSvg = styled(FavoriteSvg, {
+  position: "absolute",
+  top: "8px",
+  right: "8px",
 })
 
-export const Value = styled('h4', {
+export const HouseInfo = styled("div", {
+  padding: "6px 0 16px 16px",
+})
+
+export const Status = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  gap: 4,
+
+  marginBottom: 2,
+
+  fontWeight: "600",
+  fontSize: "12px",
+  lineHeight: "18px",
+  color: "#5BA341",
+})
+
+export const Value = styled("h4", {
+  marginBottom: 4,
+
+  fontWeight: "500",
+  fontSize: "20px",
+  lineHeight: "30px",
+  color: "$gray1",
+})
+
+export const Address = styled("p", {
   marginBottom: 8,
 
-  fontWeight: '600',
-  fontSize: '24px',
-  lineHeight: '36px',
-  color: '$gray1',
+  fontWeight: "500",
+  fontSize: "14px",
+  lineHeight: "24px",
+  color: "$gray3",
 })
 
-export const Address = styled('p', {
-  marginBottom: 14,
-
-  fontWeight: '500',
-  fontSize: '16px',
-  lineHeight: '24px',
-  color: '$gray5',
-})
-
-export const Bedrooms = styled('span', {
-  display: 'flex',
-  alignItems: 'center',
+export const Bedrooms = styled("span", {
+  display: "flex",
+  alignItems: "center",
   gap: 8,
 
-  fontWeight: '600',
-  fontSize: '16px',
-  lineHeight: '24px',
-  color: '$gray3',
+  fontWeight: "400",
+  fontSize: "14px",
+  lineHeight: "20px",
+  color: "$gray4",
 })
 
-export const Bathrooms = styled('span', {
-  display: 'flex',
-  alignItems: 'center',
+export const Bathrooms = styled("span", {
+  display: "flex",
+  alignItems: "center",
   gap: 8,
 
-  fontWeight: '600',
-  fontSize: '16px',
-  lineHeight: '24px',
-  color: '$gray3',
+  fontWeight: "400",
+  fontSize: "14px",
+  lineHeight: "20px",
+  color: "$gray4",
 })
 
-export const SquareFeet = styled('span', {
-  display: 'flex',
-  alignItems: 'center',
+export const SquareFeet = styled("span", {
+  display: "flex",
+  alignItems: "center",
   gap: 8,
 
-  fontWeight: '600',
-  fontSize: '16px',
-  lineHeight: '24px',
-  color: '$gray3',
+  fontWeight: "400",
+  fontSize: "14px",
+  lineHeight: "20px",
+  color: "$gray4",
 })
 
-export const VAMLS = styled('span', {
-  fontWeight: '500',
-  fontSize: '14px',
-  lineHeight: '24px',
-  color: '$grayW4',
-  opacity: '0.8',
+export const VAMLS = styled("span", {
+  fontWeight: "400",
+  fontSize: "10px",
+  lineHeight: "14px",
+  color: "$gray4",
+  opacity: "0.8",
 })
