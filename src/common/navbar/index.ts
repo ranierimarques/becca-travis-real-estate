bb238758333e8df60381e7d3440cc3411547f45a
Raniere Marques <Raniere852@outlook.com>
2022-08-21 08:26:33 -0300

    perf: add TalkToMe modal to dynamic import

--- src/common/navbar/index.ts
+++ src/common/navbar/index.ts
@@ -1,3 +1,2 @@
 export { HelperNavbar } from './helper-navbar/helper-navbar'
 export { MainNavbar } from './main-navbar/main-navbar'
-export { TalkToMe } from './talk-to-me/talk-to-me'
--- src/common/navbar/main-navbar/main-navbar.tsx
+++ src/common/navbar/main-navbar/main-navbar.tsx
@@ -1,10 +1,10 @@
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
-import { TalkToMe } from '../index'
 import { LogoSvg } from '../svgs'
 import * as S from './main-navbar.styles'
 
 const NavigationMenu = dynamic<{}>(() => import('../navigation-menu/navigation-menu'))
+const TalkToMe = dynamic<{}>(() => import('../talk-to-me/talk-to-me'))
 
 export function MainNavbar() {
   return (
