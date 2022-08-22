c0c62219f4c9576ca94e7eb206dbf8c3301ec345
Raniere Marques <Raniere852@outlook.com>
2022-08-22 06:13:49 -0300

    feat(our communities): update layout and create states of interactions

--- src/layout/index/our-communities/our-communities.styles.ts
+++ src/layout/index/our-communities/our-communities.styles.ts
@@ -1,12 +1,10 @@
+import Image from 'next/image'
 import { styled } from 'stitches.config'
 
 export const Section = styled('section', {
   maxWidth: 1072,
   margin: '0 auto',
   padding: '160px 0',
-
-  display: 'flex',
-  flexDirection: 'column',
 })
 
 export const Title = styled('h2', {
@@ -18,46 +16,10 @@ export const Title = styled('h2', {
   color: '$magenta6',
 })
 
-export const Heading = styled('div', {
-  width: 'fit-content',
-
-  marginBottom: 8,
-  padding: '8px 16px',
-
-  background: 'rgba($colors$magenta1Rgb, 0.1)',
-  borderRadius: 999,
-
-  fontWeight: 500,
-  fontSize: 14,
-  lineHeight: '24px',
-  color: '$magenta3',
-})
-
 export const CommunitiesList = styled('ul', {
   display: 'flex',
   flexWrap: 'wrap',
   gap: 32,
-})
-
-export const HoverSpan = styled('p', {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-
-  position: 'absolute',
-  bottom: 20,
-  left: 20,
-
-  opacity: 0,
-  visibility: 'hidden',
-
-  fontWeight: '400',
-  fontSize: '16px',
-  lineHeight: '24px',
-  color: '$white',
-  zIndex: 5,
-
-  transition: 'all ease-out 200ms',
 })
 
 export const Description = styled('span', {
@@ -69,35 +31,82 @@ export const Description = styled('span', {
   fontSize: 20,
   lineHeight: '30px',
   color: '$white',
-  zIndex: 3,
 
-  transition: 'all ease-out 200ms',
-})
-
-export const Community = styled('li', {
-  position: 'relative',
-  width: '336px',
-  height: '229px',
-  borderRadius: 8,
-  overflow: 'hidden',
-
-  '&:hover': {
-    [`& ${HoverSpan}`]: {
-      opacity: 1,
-      visibility: 'visible',
-    },
-
-    [`& ${Description}`]: {
-      bottom: 48,
-    },
+  '@media (prefers-reduced-motion: no-preference)': {
+    transition: 'transform 350ms ease',
   },
 })
 
+export const HoverSpan = styled('p', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  position: 'absolute',
+  bottom: 20,
+  left: 20,
+
+  fontWeight: '400',
+  fontSize: '16px',
+  lineHeight: '24px',
+  color: '$white',
+  textDecoration: 'underline',
+
+  opacity: 0,
+  transform: 'scale(0.5)',
+  transformOrigin: 'left',
+
+  '@media (prefers-reduced-motion: no-preference)': {
+    transition: 'transform 300ms ease, opacity 300ms ease',
+  },
+})
+
+export const CommunityImage = styled(Image, {
+  borderRadius: '8px',
+
+  '@media (prefers-reduced-motion: no-preference)': {
+    transition: 'filter 300ms ease',
+  },
+})
+
+export const CommunityLink = styled('a', {
+  display: 'block',
+
+  position: 'relative',
+
+  width: 336,
+  height: 229,
+
+  borderRadius: 8,
+
+  '&:hover': {
+    [`& ${CommunityImage}`]: { filter: 'brightness(0.8)' },
+    [`& ${HoverSpan}`]: { transform: 'scale(1)', opacity: 1 },
+    [`& ${Description}`]: {
+      transform: 'translateY(-28px)',
+      '@media (prefers-reduced-motion: no-preference)': {
+        transition: 'transform 200ms ease',
+      },
+    },
+  },
+  '&:focus-visible': {
+    boxShadow: '0 0 0 4px $colors$magenta9',
+    [`& ${CommunityImage}`]: { filter: 'brightness(0.8)' },
+    [`& ${HoverSpan}`]: { transform: 'scale(1)', opacity: 1 },
+    [`& ${Description}`]: {
+      transform: 'translateY(-28px)',
+      '@media (prefers-reduced-motion: no-preference)': {
+        transition: 'transform 200ms ease',
+      },
+    },
+  },
+  '&:active': { [`& ${CommunityImage}`]: { filter: 'brightness(0.7)' } },
+})
+
 export const ImageMask = styled('div', {
+  position: 'absolute',
+  inset: 0,
+  borderRadius: '8px',
   background:
     'linear-gradient(180deg, rgba($colors$gray2Rgb, 0) 50%, rgba($colors$blackRgb, 0.6) 100%)',
-  position: 'absolute',
-  width: '100%',
-  height: '100%',
-  zIndex: 2,
 })
