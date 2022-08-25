a06b0e3189af3e186a1518a648ea07570dca243c
Raniere Marques <Raniere852@outlook.com>
2022-08-25 20:18:39 -0300

    fix(last call): zIndex

--- src/shared/last-call/last-call.styles.ts
+++ src/shared/last-call/last-call.styles.ts
@@ -1,59 +1,58 @@
-import { css, styled } from "stitches.config"
+import { css, styled } from 'stitches.config'
 
-export const Section = styled("section", {
-  margin: "0 auto",
-  maxWidth: "1072px",
-  padding: "120px 0",
+export const Section = styled('section', {
+  margin: '0 auto',
+  maxWidth: '1072px',
+  padding: '120px 0',
 })
 
-export const Container = styled("div", {
-  display: "flex",
-  alignItems: "center",
+export const Container = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
 
-  position: "relative",
+  position: 'relative',
 
-  padding: "64px 0 64px 64px",
+  padding: '64px 0 64px 64px',
 
-  background: "rgba($colors$tangerine10Rgb, 0.75)",
+  background: 'rgba($colors$tangerine10Rgb, 0.75)',
   boxShadow:
-    "0px 14px 50px rgba($colors$blackRgb, 0.08), 0 0 0 1px rgba($colors$tangerine7Rgb, 0.15)",
-  borderRadius: "24px",
+    '0px 14px 50px rgba($colors$blackRgb, 0.08), 0 0 0 1px rgba($colors$tangerine7Rgb, 0.15)',
+  borderRadius: '24px',
 })
 
-export const Title = styled("h3", {
-  maxWidth: "262px",
-  marginBottom: "20px",
+export const Title = styled('h3', {
+  maxWidth: '262px',
+  marginBottom: '20px',
 
-  fontWeight: "600",
-  fontSize: "32px",
-  lineHeight: "44px",
-  color: "$gray1",
+  fontWeight: '600',
+  fontSize: '32px',
+  lineHeight: '44px',
+  color: '$gray1',
 })
 
-export const Conversation = styled("span", {
-  fontWeight: "600",
-  fontSize: "32px",
-  lineHeight: "44px",
-  color: "$magenta1",
+export const Conversation = styled('span', {
+  fontWeight: '600',
+  fontSize: '32px',
+  lineHeight: '44px',
+  color: '$magenta1',
 })
 
-export const Paragraph = styled("p", {
-  maxWidth: "308px",
-  marginBottom: "32px",
+export const Paragraph = styled('p', {
+  maxWidth: '308px',
+  marginBottom: '32px',
 
-  fontWeight: "300",
-  fontSize: "16px",
-  lineHeight: "29px",
-  color: "$details1",
+  fontWeight: '300',
+  fontSize: '16px',
+  lineHeight: '29px',
+  color: '$details1',
 })
 
-export const IconContainer = styled("div", {
-  position: "absolute",
-  padding: "10px",
-  filter: "drop-shadow(0px 12px 15px rgba($colors$tangerine8Rgb, 0.2))",
-  zIndex: 2,
+export const IconContainer = styled('div', {
+  position: 'absolute',
+  padding: '10px',
+  filter: 'drop-shadow(0px 12px 15px rgba($colors$tangerine8Rgb, 0.2))',
 })
 
 export const SvgStyles = css({
-  display: "block",
+  display: 'block',
 })
