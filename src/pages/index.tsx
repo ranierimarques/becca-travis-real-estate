60e1cd7642b9e48c1145b05a2a750e138dadda48
Raniere Marques <Raniere852@outlook.com>
2022-09-01 17:19:09 -0300

    feat: test 9

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       data,
     },
-    revalidate: 60 * 30, // 30 minutes
+    // revalidate: 60 * 30, // 30 minutes
   }
 }
 
