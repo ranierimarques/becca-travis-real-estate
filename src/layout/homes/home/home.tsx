d4bed5d34658d807b67e13c9e1968c1020f3763d
Raniere Marques <Raniere852@outlook.com>
2022-09-02 20:06:24 -0300

    feat: refactor logic of renderrer 3/3

--- src/layout/homes/home/home.tsx
+++ src/layout/homes/home/home.tsx
@@ -1,26 +1,35 @@
-import { Box, Flex } from "@common"
-import { LastCall } from "@shared"
-import { Slider } from "../"
-import { AskAQuestion } from "./ask-a-question/ask-a-question"
-import * as S from "./home.styles"
-import { PropertyDetails } from "./property-details/property-details"
-import { HeartSvg, ShareSvg } from "./svgs"
+import { Box, Flex } from '@common'
+import { LastCall } from '@shared'
+import { Slider } from '../'
+import { AskAQuestion } from './ask-a-question/ask-a-question'
+import * as S from './home.styles'
+import { PropertyDetails } from './property-details/property-details'
+import { HeartSvg, ShareSvg } from './svgs'
 
-export function Home({ data }: any) {
-  function formatToDollar(amount: number) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      maximumFractionDigits: 0,
-    }).format(amount)
+interface Listing {
+  listing: {
+    price: string
+    address: string
+    status: string
+    lastUpdated: string
+    media: string[]
+    bedroomsTotal: number
+    bathroomsTotal: number
+    lotSizeSquareFeet: string
+    subdivisionName: string
+    propertySubType: string
+    countyOrParish: string
+    cityRegion: string | null
   }
+}
 
+export function Home({ listing }: Listing) {
   return (
     <S.Section>
       <div>
         <S.Header>
           <Flex align="center" justify="between" css={{ mb: 16 }}>
-            <S.HouseName>{data.UnparsedAddress}</S.HouseName>
+            <S.HouseName>{listing.address}</S.HouseName>
             <S.ShareAndSave>
               <S.Button>
                 <ShareSvg />
@@ -35,37 +44,32 @@ export function Home({ data }: any) {
           </Flex>
 
           <Flex align="center" css={{ gap: 16 }}>
-            <S.HousePrice>{formatToDollar(data.ListPrice)}</S.HousePrice>
-            <Box
-              css={{ h: 16, w: 2, background: "rgba($colors$gray5Rgb, .5)" }}
-            />
+            <S.HousePrice>{listing.price}</S.HousePrice>
+            <Box css={{ h: 16, w: 2, background: 'rgba($colors$gray5Rgb, .5)' }} />
             <S.HouseStatus>
-              Status: {""}
-              <S.Status>{data.StandardStatus}</S.Status>
+              Status:
+              <S.Status>{listing.status}</S.Status>
             </S.HouseStatus>
 
             <S.Dot />
 
             <S.HouseStatus>
-              On site: {""}
-              <S.Status>
-                {data.DaysOnMarket}
-                10 days
-              </S.Status>
+              On site:
+              <S.Status>10 days</S.Status>
             </S.HouseStatus>
 
             <S.Dot />
 
             <S.HouseStatus>
-              Updated: {""}
-              <S.Status>{data.BridgeModificationTimestamp}</S.Status>
+              Updated:
+              <S.Status>{listing.lastUpdated}</S.Status>
             </S.HouseStatus>
           </Flex>
         </S.Header>
 
         <Flex justify="between" css={{ mb: 32 }}>
-          <Slider data={data} />
-          <PropertyDetails data={data} />
+          <Slider media={listing.media} />
+          <PropertyDetails listing={listing} />
         </Flex>
       </div>
 
--- src/pages/homes/[id].tsx
+++ src/pages/homes/[id].tsx
@@ -1,16 +1,18 @@
 import { Home } from '@layout/homes'
-import { GetStaticPaths, GetStaticProps } from 'next'
+import convertSquareFeets from '@resources/utils/convert-square-feets'
+import formatToDollar from '@resources/utils/format-to-dollar'
+import InferNextPropsType from 'infer-next-props-type'
+import { GetStaticPaths, NextPage } from 'next'
 import Head from 'next/head'
 
-export default function HomePage({ bundle }: any) {
+const Page: NextPage<InferNextPropsType<typeof getStaticProps>> = ({ listing }) => {
   return (
     <main>
       <Head>
         <title>Becca Travis</title>
       </Head>
 
-      {/* <Home data={bundle} /> */}
-      {JSON.stringify(bundle, null, 2)}
+      <Home listing={listing} />
     </main>
   )
 }
@@ -22,21 +24,83 @@ export const getStaticPaths: GetStaticPaths = () => {
   }
 }
 
+type Params = {
+  params: {
+    id: string
+  }
+}
+
+type FetchTypes = {
+  success: boolean
+  status: number
+  bundle: {
+    LotSizeSquareFeet: number
+    BedroomsTotal: number
+    BridgeModificationTimestamp: string
+    MlsStatus:
+      | 'ACTIVE'
+      | 'Active – Non Compliance'
+      | 'Cancelled'
+      | 'Coming Soon'
+      | 'Contingent'
+      | 'Expired'
+      | 'Expired Provisional'
+      | 'Leased'
+      | 'Office Exclusive'
+      | 'Pending'
+      | 'Provisional'
+      | 'Sold'
+      | 'Temporarily Off Market'
+    Media: {
+      MediaURL: string
+    }[]
+    ListingId: string
+    ListPrice: number
+    BathroomsTotalInteger: number
+    UnparsedAddress: string
+    ListingKey: string
+    FeedTypes: []
+    PropertySubType: string
+    CountyOrParish: string
+    SubdivisionName: string
+    CityRegion: string | null
+    url: string
+  }
+}
+
+const endpoint = 'https://api.bridgedataoutput.com/api/v2/valleymls/listings/'
+
 const options = {
   method: 'GET',
   headers: { Authorization: 'Bearer c8c61ffc7e3cfcb91714551392eb82cd' },
-}
+} as RequestInit
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const res = await fetch(
-    `https://api.bridgedataoutput.com/api/v2/valleymls/listings?ListingId=${params?.id}`,
-    options
-  )
-  const data = await res.json()
+export const getStaticProps = async ({ params }: Params) => {
+  const response = await fetch(endpoint + params.id, options)
+  const data = (await response.json()) as FetchTypes
 
-  if (data.total === 0) {
-    return { notFound: true }
+  if (!data.success) return { notFound: true }
+
+  const listing = {
+    price: formatToDollar(data.bundle.ListPrice),
+    address: data.bundle.UnparsedAddress,
+    status: data.bundle.MlsStatus,
+    lastUpdated: data.bundle.BridgeModificationTimestamp,
+    media: data.bundle.Media.map(media => media.MediaURL),
+    bathroomsTotal: data.bundle.BathroomsTotalInteger,
+    bedroomsTotal: data.bundle.BedroomsTotal,
+    lotSizeSquareFeet: convertSquareFeets(data.bundle.LotSizeSquareFeet),
+    subdivisionName: data.bundle.SubdivisionName,
+    propertySubType: data.bundle.PropertySubType,
+    countyOrParish: data.bundle.CountyOrParish,
+    cityRegion: data.bundle.CityRegion,
   }
 
-  return { props: { bundle: data.bundle[0] } }
+  return {
+    props: {
+      listing,
+    },
+  }
 }
+
+export default Page
