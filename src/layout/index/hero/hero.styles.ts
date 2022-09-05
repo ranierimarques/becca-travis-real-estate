e8f619b6786b6b9ce6fcdaa80cd6d9b1cd3a8c8f
Pedro Renan <prenansb@gmail.com>
2022-09-05 14:34:37 -0300

    feat(hero): implementing input search

--- src/layout/index/hero/hero.styles.ts
+++ src/layout/index/hero/hero.styles.ts
@@ -1,99 +1,99 @@
-import { styled } from "stitches.config"
-import { ArrowDown as ArrowDownSvg } from "./svgs"
+import { styled } from 'stitches.config'
+import { ArrowDown as ArrowDownSvg } from './svgs'
 
-export const Section = styled("section", {
-  position: "relative",
+export const Section = styled('section', {
+  position: 'relative',
 
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 })
 
-export const Title = styled("h1", {
+export const Title = styled('h1', {
   marginTop: 120,
 
   fontWeight: 500,
   fontSize: 40,
-  lineHeight: "56px",
-  textAlign: "center",
-  color: "$grayW10",
+  lineHeight: '56px',
+  textAlign: 'center',
+  color: '$grayW10',
 })
 
-export const InputWrapper = styled("div", {
+export const InputWrapper = styled('div', {
   marginTop: 32,
-  paddingLeft: "24px",
+  paddingLeft: '24px',
 
-  background: "$offWhite2",
+  background: '$offWhite2',
 
-  display: "flex",
-  alignItems: "center",
+  display: 'flex',
+  alignItems: 'center',
   gap: 16,
 
   borderRadius: 8,
   width: 527,
 })
 
-export const SearchInput = styled("input", {
+export const SearchInput = styled('input', {
   flex: 1,
 
   fontWeight: 600,
   fontSize: 14,
-  lineHeight: "18px",
-  color: "$gray5",
-  caretColor: "$magenta8",
+  lineHeight: '18px',
+  color: '$gray5',
+  caretColor: '$magenta8',
 
-  "&::placeholder": {
+  '&::placeholder': {
     fontWeight: 400,
-    color: "$grayW5",
+    color: '$grayW5',
   },
 })
 
-export const SearchButton = styled("button", {
-  padding: "20px 15px",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
+export const SearchButton = styled('button', {
+  padding: '20px 15px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 
-  background: "$grayW9",
-  borderRadius: "0px 8px 8px 0px",
+  background: '$grayW9',
+  borderRadius: '0px 8px 8px 0px',
 })
 
-export const HelperText = styled("span", {
-  margin: "24px 0 116px",
+export const HelperText = styled('span', {
+  margin: '24px 0 116px',
 
   fontWeight: 400,
   fontSize: 14,
-  lineHeight: "26px",
-  textAlign: "center",
-  color: "$grayW10",
+  lineHeight: '26px',
+  textAlign: 'center',
+  color: '$grayW10',
 })
 
-export const ImageWrapper = styled("div", {
-  position: "absolute",
+export const ImageWrapper = styled('div', {
+  position: 'absolute',
   top: 0,
-  width: "100%",
-  height: "100%",
+  width: '100%',
+  height: '100%',
   zIndex: -1,
-  userSelect: "none",
-  pointerEvents: "none",
+  userSelect: 'none',
+  pointerEvents: 'none',
 })
 
-export const ImageOverlay = styled("div", {
-  position: "absolute",
+export const ImageOverlay = styled('div', {
+  position: 'absolute',
   top: 0,
-  width: "100%",
-  height: "100%",
-  background: "$details1",
+  width: '100%',
+  height: '100%',
+  background: '$details1',
   opacity: 0.7,
 })
 
 export const ArrowDown = styled(ArrowDownSvg, {
-  boxSizing: "content-box",
+  boxSizing: 'content-box',
   padding: 6,
-  background: "$white",
-  boxShadow: "0px 5px 32px rgba($colors$grayW2Rgb, 0.5)",
+  background: '$white',
+  boxShadow: '0px 5px 32px rgba($colors$grayW2Rgb, 0.5)',
   borderRadius: 999,
 
-  position: "absolute",
+  position: 'absolute',
   bottom: -16,
 })
--- src/layout/index/hero/hero.tsx
+++ src/layout/index/hero/hero.tsx
@@ -1,11 +1,77 @@
-import Image from "next/image"
-import * as S from "./hero.styles"
-import { downtownHuntsville } from "./images"
-import { Loupe } from "./svgs"
+import { useDebounce } from '@resources/hooks/useDebounce'
+import { useFetch } from '@resources/hooks/useFetch'
+import Image from 'next/image'
+import Script from 'next/script'
+import { FormEvent, useEffect, useLayoutEffect, useState } from 'react'
+import { usePlacesWidget } from 'react-google-autocomplete'
+import * as S from './hero.styles'
+import { downtownHuntsville } from './images'
+import { Loupe } from './svgs'
 
 export function Hero() {
+  const [inputValue, setInputValue] = useState('')
+  const [searchValue, setSearchValue] = useState('')
+  const suggestions: any[] = []
+
+  // useLayoutEffect(() => {
+  //   const displaySuggestions = function (predictions: any, status: any) {
+  //     if (status != google.maps.places.PlacesServiceStatus.OK || !predictions) {
+  //       alert(status)
+  //       return
+  //     }
+
+  //     predictions.forEach((prediction: any) => {
+  //       suggestions.push(prediction)
+  //     })
+  //   }
+
+  //   const service = new google.maps.places.AutocompleteService()
+
+  //   service.getQueryPredictions({ input: 'pizza near Syd' }, displaySuggestions)
+  // }, [])
+
+  const { ref: searchRef } = usePlacesWidget({
+    apiKey: 'AIzaSyAkjtR2QOQxzSixdT3RJXHI3ohZ90yIRjw',
+    onPlaceSelected: place => {
+      console.log(place)
+      setSearchValue(place.address_components[0].long_name)
+    },
+    options: {
+      types: ['street_address', 'neighborhood', 'locality', 'sublocality', 'postal_code'],
+      componentRestrictions: { country: 'us' },
+    },
+  })
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    const endpoint = `https://api.bridgedataoutput.com/api/v2/OData/valleymls/Property?access_token=c8c61ffc7e3cfcb91714551392eb82cd&$filter=contains(tolower(UnparsedAddress),'${searchValue}')`
+    const data = await fetch(endpoint)
+    const result = await data.json()
+    console.log(result)
+  }
+
+  function handleMyLocation(event: FormEvent) {
+    event.preventDefault()
+    navigator.geolocation.getCurrentPosition(
+      async position => {
+        const { latitude, longitude } = position.coords
+        const endpoint = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCVaTtpBS5SweU8_yF0aL2Wm-58g8PTix8`
+        const response = await fetch(endpoint)
+        const data = await response.json()
+        console.log(data)
+      },
+      err => {
+        window.alert(
+          'There is no location support on this device or it is disabled. Please check your settings.'
+        )
+      }
+    )
+  }
+
   return (
     <S.Section>
+      <Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkjtR2QOQxzSixdT3RJXHI3ohZ90yIRjw&libraries=places" />
+
       <S.Title>
         Find your dream <br />
         home today
@@ -13,12 +79,20 @@ export function Hero() {
 
       <S.InputWrapper>
         <S.SearchInput
+          ref={searchRef}
           type="text"
           placeholder="Search by address, neighborhood, city or ZIP code"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
         />
-        <S.SearchButton>
+        <S.SearchButton onClick={handleSubmit}>
           <Loupe />
         </S.SearchButton>
+        <button onClick={handleMyLocation}>My location</button>
+        <div>
+          {suggestions.length > 0 &&
+            suggestions.map(suggestion => <span key={suggestion}>{suggestion}</span>)}
+        </div>
       </S.InputWrapper>
 
       <S.HelperText>
