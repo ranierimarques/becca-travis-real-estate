df1db64e7139f11de6726c654f521b2125b99c8a
Raniere Marques <Raniere852@outlook.com>
2022-09-19 13:54:03 -0300

    build: config eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        arrowParens: 'avoid',
+        singleQuote: true,
+        tabWidth: 2,
+        semi: false,
+        printWidth: 90,
+      },
+    ],
+  },
+}
