344b6ab6ef35016850279442c9c56803ce713b5b
Raniere Marques <Raniere852@outlook.com>
2022-10-12 13:13:42 -0300

    refactor: remove 1 export

--- src/components/primitives/dialog/dialog.styles.ts
+++ src/components/primitives/dialog/dialog.styles.ts
@@ -6,7 +6,7 @@ const overlayShow = keyframes({
   '100%': { opacity: 1 },
 })
 
-export const contentShow = keyframes({
+const contentShow = keyframes({
   '0%': { opacity: 0, transform: 'translate(-50%, -48%) scale(.96)' },
   '100%': { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
 })
