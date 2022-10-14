43d17790f78b89bc36a899631cd3831c2d6fa88f
Raniere Marques <Raniere852@outlook.com>
2022-10-14 03:13:12 -0300

    feat: make style updates in home search page

--- src/layout/homes/sections/homes/search/search.styles.ts
+++ src/layout/homes/sections/homes/search/search.styles.ts
@@ -1,1 +1,54 @@
 import { styled } from 'stitches.config'
+
+export const Container = styled('div', {
+  margin: '20px 16px',
+})
+
+export const HomesForSale = styled('div', {
+  fontWeight: '400',
+  fontSize: '14px',
+  lineHeight: '24px',
+  letterSpacing: '-0.02em',
+  color: '$magenta6',
+})
+
+export const LastUpdate = styled('div', {
+  fontWeight: '400',
+  fontSize: '14px',
+  lineHeight: '24px',
+  letterSpacing: '-0.02em',
+  color: '$grayW5',
+})
+
+export const Button = styled('button', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: 8,
+
+  fontWeight: '500',
+  fontSize: '14px',
+  lineHeight: '21px',
+  color: '$grayW4',
+
+  borderRadius: '5px',
+  padding: '10px 16px',
+  boxShadow: 'inset 0 0 0 1px $colors$grayW4',
+
+  variants: {
+    active: {
+      true: {
+        background: '$grayW3',
+        boxShadow: 'inset 0 0 0 1px $colors$grayW3',
+        color: '$white',
+      },
+    },
+    borderDirection: {
+      left: {
+        borderRadius: '5px 0 0 5px',
+      },
+      right: {
+        borderRadius: '0 5px 5px 0',
+      },
+    },
+  },
+})
--- src/layout/homes/sections/homes/search/search.tsx
+++ src/layout/homes/sections/homes/search/search.tsx
@@ -1,5 +1,30 @@
+import { Flex } from '@common'
+import { SearchInput } from '@shared'
+import * as Svg from '../svgs'
 import * as S from './search.styles'
 
 export function Search() {
-  return <div>Hello world</div>
+  return (
+    <S.Container>
+      <Flex justify="between">
+        <S.HomesForSale>3208 Homes for sale</S.HomesForSale>
+        <S.LastUpdate>Last update: 30 minutes ago</S.LastUpdate>
+      </Flex>
+      <SearchInput variant="houses" />
+      <Flex justify="between" css={{ mt: 16 }}>
+        <Flex>
+          <S.Button borderDirection="left" active>
+            <Svg.Location /> Map
+          </S.Button>
+          <S.Button borderDirection="right">
+            <Svg.Gallery /> Gallery
+          </S.Button>
+        </Flex>
+
+        <S.Button>
+          <Svg.Filter /> Filter
+        </S.Button>
+      </Flex>
+    </S.Container>
+  )
 }
