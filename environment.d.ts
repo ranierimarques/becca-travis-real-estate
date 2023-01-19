86078ab4ecd701f91b53d32c82fd00337c541d95
Raniere Marques <Raniere852@outlook.com>
2023-01-18 22:05:25 -0300

    feat: add types from .env file

---
+++ environment.d.ts
@@ -0,0 +1,16 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    // Public
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string
+    NEXT_PUBLIC_SMARTY_AUTOCOMPLETE_API_KEY: string
+    NEXT_PUBLIC_BING_AUTOCOMPLETE_API_KEY: string
+    NEXT_PUBLIC_WEB3FORMS_API_KEY: string
+    NEXT_PUBLIC_BRIDGE_API_KEY: string
+
+    // Server only
+    GOOGLE_GEOLOCATION_API_KEY: string
+    BRIDGE_API_KEY: string
+    REVALIDATE_API_SECRET_TOKEN: string
+    YELP_API_KEY: string
+  }
+}
