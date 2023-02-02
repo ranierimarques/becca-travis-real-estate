b19f29d820cc0baeebd15295afdc0e64c651a066
Pedro Renan <prenansb@gmail.com>
2023-02-01 23:49:07 -0300

    feat(contact us): responsive finished

--- src/layout/contact-us/sections/hero/contact-information/contact-information.styles.ts
+++ src/layout/contact-us/sections/hero/contact-information/contact-information.styles.ts
@@ -13,12 +13,38 @@ export const Card = styled('div', {
   overflow: 'hidden',
 
   background: 'linear-gradient(180deg, #FFFFFF 0%, #F6F6F8 100%)',
+
+  '@bp5': {
+    minWidth: '286px',
+  },
+
+  '@bp4': {
+    width: '100%',
+    background: 'transparent',
+    boxShadow: 'none',
+    minWidth: 'auto',
+  },
+})
+
+export const CardContainer = styled('div', {
+  p: '32px 0 0 32px',
+  '@bp4': {
+    padding: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
 })
 
 export const Svg = styled(BackgroundSvg, {
   position: 'absolute',
   left: 0,
   bottom: 0,
+
+  '@bp4': {
+    display: 'none',
+  },
 })
 
 export const Title = styled('h4', {
@@ -28,6 +54,10 @@ export const Title = styled('h4', {
   fontSize: '22px',
   lineHeight: '32px',
   color: '$grayW2',
+
+  '@bp5': {
+    fontSize: '20px',
+  },
 })
 
 export const Description = styled('p', {
@@ -38,6 +68,24 @@ export const Description = styled('p', {
   fontSize: '14px',
   lineHeight: '22px',
   color: '$grayW4',
+
+  '@bp5': {
+    marginBottom: 24,
+
+    fontSize: '12px',
+    lineHeight: '20px',
+  },
+  '@bp4': {
+    marginBottom: 32,
+
+    fontSize: '14px',
+    lineHeight: '26px',
+    maxWidth: 'inherit',
+  },
+
+  '@bp3': {
+    marginBottom: 24,
+  },
 })
 
 export const Contact = styled('div', {
