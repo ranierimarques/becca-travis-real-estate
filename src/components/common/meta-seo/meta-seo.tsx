54b593313b632713b8ffa5dc399e41c33cfec65a
Pedro Renan <prenansb@gmail.com>
2023-03-10 13:47:41 -0300

    fix(og image): name changed

--- src/components/common/meta-seo/meta-seo.tsx
+++ src/components/common/meta-seo/meta-seo.tsx
@@ -21,7 +21,7 @@ export function MetaSEO({ title, description, image, alt, url, children }: MetaS
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:locale" content="pt_BR" />
-      <meta property="og:site_name" content="Octane Studio" />
+      <meta property="og:site_name" content="Becca Travis" />
       <meta property="og:image" content={image} />
       <meta property="og:image:secure_url" content={image} />
       <meta property="og:image:alt" content={alt} />
