e2eed63e97ee4e3cabd25842e27270e157f8cef7
Raniere Marques <Raniere852@outlook.com>
2023-05-16 02:17:26 -0300

    fix: get-cookie

--- src/resources/utils/cookies/get-cookie.ts
+++ src/resources/utils/cookies/get-cookie.ts
@@ -1,6 +1,6 @@
 export function getCookie(cookieName: string) {
   const name = cookieName + '='
-  const cookies = document.cookie.split(';')
+  const cookies = document.cookie.split('; ')
 
   for (const cookie of cookies) {
     if (cookie.indexOf(name) === 0) {
