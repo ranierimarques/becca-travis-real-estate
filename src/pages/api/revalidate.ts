e43838a4d25450bec2b5795690d207bc0e3af30c
Raniere Marques <Raniere852@outlook.com>
2023-05-23 17:12:47 -0300

    fix: test 11

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -54,6 +54,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     } catch (err) {
       // If there was an error, Next.js will continue
       // to show the last successfully generated page
+      console.log(err)
       return res.status(500).send('Error revalidating')
     }
   }
