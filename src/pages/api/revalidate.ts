80e2a232de9ab304eadded72df94953eed6a5e6f
Raniere Marques <Raniere852@outlook.com>
2023-05-23 17:01:19 -0300

    fix: test 7

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -44,9 +44,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.headers.type === 'utils') {
     try {
       console.log('teste')
-      console.log(req.body.data.slug)
-      console.log(JSON.stringify(req.body.data.slug))
-      console.log(JSON.parse(req.body).data.slug)
+      console.log('a', req.body.data.slug)
+      console.log('b', JSON.stringify(req.body.data.slug))
+      console.log('c', JSON.parse(req.body).data.slug)
 
       await res.revalidate(`/${JSON.parse(req.body).data.slug}`)
       return res.json({ revalidated: true })
