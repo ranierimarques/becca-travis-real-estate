f08975789d72a3e966f74d585ffb9bfc2275bd31
Raniere Marques <Raniere852@outlook.com>
2023-05-23 11:09:32 -0300

    build: add next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,13 @@
-const generatedUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-const url = `${generatedUrl ? 'https://' + generatedUrl : 'http://localhost:3000/'}`
+const environment = process.env.NEXT_PUBLIC_VERCEL_ENV
+const vercelURL = process.env.NEXT_PUBLIC_VERCEL_URL
+
+const url = environment === 'production' ? 'beccatravis.com' : vercelURL
+const fullURL = `${url ? 'https://' + url : 'http://localhost:3000'}`
 
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: url,
+  siteUrl: fullURL,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 }
